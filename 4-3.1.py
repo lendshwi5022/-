30d4422dd0b60e8c0a65051d644032dbdae35b4d
lendshwi5022 <lendshwi5022@gmail.com>
2021-07-08 14:40:45 +0800

    first commit

---
+++ 4-3.1.py
@@ -0,0 +1,4 @@
+temperature = input('請輸入攝氏溫度: ')
+temperature = float(temperature)
+f = temperature * 9 / 5 + 32
+print('華氏溫度為: ', f)
---
+++ 4-3.py
@@ -0,0 +1,4 @@
+temperature = input('請輸入溫度:  ')
+temperature = float(temperature)
+f = temperature * 9 / 5 + 32
+print(f)
